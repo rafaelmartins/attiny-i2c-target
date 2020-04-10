2d79b04ad2294c29e64ff7edfa089f51d887f524
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2020-04-10 23:15:53 +0200

    use dwtk-makefile

---
+++ dwtk-config.mk
@@ -0,0 +1,28 @@
+AVR_MCU ?= attiny84a
+AVR_F_CPU ?= 8000000UL
+
+AVR_LFUSE ?= 0xE2
+AVR_HFUSE ?= 0xDF
+
+FIRMWARE_CFLAGS = \
+	-DI2C_REGISTER_ALLOC=1 \
+	-Wno-implicit-fallthrough \
+	$(NULL)
+
+FIRMWARE_SOURCES = \
+	test.c \
+	i2c-device.c \
+	$(NULL)
+
+FIRMWARE_HEADERS = \
+	i2c-device.h \
+	$(NULL)
+
+build-test:
+	set -e; \
+	for i in {2,4,8}4{,a} {2,4,8}5 2313{,a} 4313; do \
+		$(MAKE) clean all AVR_MCU=attiny$$i; \
+	done
+	$(MAKE) clean
+
+.PHONY: build-test
